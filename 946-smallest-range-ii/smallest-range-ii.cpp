63ab6634dc5bb4ee54c2a57180599748827951a1
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2025-05-02 11:03:56 +0530

    Time: 3 ms (55.22%) | Memory: 19.2 MB (71.94%) - LeetSync

--- 946-smallest-range-ii/smallest-range-ii.cpp
+++ 946-smallest-range-ii/smallest-range-ii.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int smallestRangeII(vector<int>& nums, int k) {
         int n = nums.size();
+        if(n == 1){
+            return 0;
+        }
         sort(nums.begin(), nums.end());
         int res = nums[n-1] - nums[0];
         for(int i=0;i<n - 1;i++){
