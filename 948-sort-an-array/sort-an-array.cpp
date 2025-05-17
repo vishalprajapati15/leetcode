9e0b1c51508209f3045f1eeb815fa6d4c50432aa
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2025-05-17 17:42:32 +0530

    Time: 96 ms (59.77%) | Memory: 99.1 MB (46.71%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
+        for(int &num : nums){
+            mp[num]++;
+        }   
+        int mini = *min_element(begin(nums), end(nums));
+        int maxi = *max_element(begin(nums), end(nums));
+
+        int i=0;
+
+        for(int num = mini; num<=maxi; num++){
+            while(mp[num] > 0){
+                nums[i++] = num;
+                mp[num]--;
+            }
+        }
         return nums;
     }
 };
