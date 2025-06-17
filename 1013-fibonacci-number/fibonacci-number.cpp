ec993c18c53e7e6ec7536eda6000c84089bca028
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2025-06-17 10:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (64.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,6 +7,12 @@ public:
         if(n==1){
             return 1;
         }
-        return fib(n-1) + fib(n-2);
+        int a = 0, b = 1;
+        for(int i=2;i<=n;i++){
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 };
