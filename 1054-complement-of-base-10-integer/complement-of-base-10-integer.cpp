e0b66cba7464871e50c521d9d1a257c7fcc9f154
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2025-06-20 15:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (23.44%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,13 +3,13 @@ public:
     int bitwiseComplement(int n) {
         int m = n;
         if(n == 0){
-            return 1;
+            return 1;       // edge case
         }
         int mask = 0;
         while(m != 0){
-            mask = (mask << 1) | 1;
-            m = m >> 1;
+            mask = (mask << 1) | 1;         //create mask by left shift ans OR with 1;
+            m = m >> 1;                     // m ko right shift krke 0 kiya 
         }
-        return (~n) & mask;
+        return (~n) & mask;                 // not of n AND with mask ; 
     }
 };
