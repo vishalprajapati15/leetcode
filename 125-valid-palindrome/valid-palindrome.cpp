e9146b216e6f65b21016a5b4d1fae6cc0e4134f4
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2025-08-09 10:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (58.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0;
-        int right = s.size();
+        int right = s.size()-1;
         while(left < right){
             if(!isalnum(s[left])){
                 left++;
