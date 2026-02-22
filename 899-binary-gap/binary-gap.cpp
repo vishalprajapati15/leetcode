1390c250e7137814ecfd7089974f167ee7b58cff
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-02-22 19:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int curr =0;
+        int prev = -1;
+        
+        int res = 0;
+        while(n > 0){
+            if((n&1)>0){
+                res = (prev != -1) ? max(res, curr - prev) : res;
+                prev = curr;
+            }
+            curr++;
+            n >>= 1;
+        }
+        return res;
+    }
+};
