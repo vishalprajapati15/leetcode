197eb5ba2e8bd7083eeafd94b70615d929c03ef3
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-03-10 11:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (39.88%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string>words;
+        string word;
+        stringstream ss(s);
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        if(pattern.length() != words.size()){
+            return false;
+        }
+
+        unordered_map<char, string>mp1;
+        unordered_map<string, char>mp2;
+
+        for(int i=0;i<pattern.length(); i++){
+            char p = pattern[i];
+            string w = words[i];
+            if(mp1.count(p) && mp1[p] != w){
+                return false;
+            }
+            if(mp2.count(w) && mp2[w] != p){
+                return false;
+            }
+            mp1[p] = w;
+            mp2[w] = p;
+        }
+        return true;
+    }
+};
