f6be9f7e0772a9ad6d550691fb333f3290b8bada
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-03-16 16:31:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (94.99%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         sort(s.begin(), s.end());
-        sort(t.begin(), t.end()); // Fixed: t.end() instead of t.begin()
+        sort(t.begin(), t.end());
         
-        // Loop through the shorter string
         for (int i = 0; i < s.size(); i++) {
             if (s[i] != t[i]) {
-                return t[i]; // Found the difference early
+                return t[i];
             }
         }
         
