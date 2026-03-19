89132f29ff1d629147ccb9c08fa1a0117cc22890
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-03-19 14:20:40 +0000

    PYQ 4

---
+++ Q4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout <<"No of bulbs: ";
+    cin>>n;
+    int ans = sqrt(n);
+    cout<<"After n round: "<<ans<<endl;
+    count<<"Done";
+    return 0;
