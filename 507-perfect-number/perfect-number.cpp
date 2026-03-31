b846090cce10478139fef138a2ed39f96e2cbff0
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-03-31 16:17:00 +0530

    Time: 1045 ms (49.69%) | Memory: 7.8 MB (19.98%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int n = num;
+        int sum =0;
+        for(int i=1;i<=n/2;i++){
+            if(n % i ==0){
+                sum += i;
+            }
+        }
+
+        return num == sum;
+    }
+};
