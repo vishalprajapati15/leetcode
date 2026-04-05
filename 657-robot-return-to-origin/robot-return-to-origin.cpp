665dcebf44113be86132fe3340503872642297f9
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-04-06 00:07:57 +0530

    Time: 8 ms (10.20%) | Memory: 10.3 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x =0, y = 0;
+
+        for(char ch : moves){
+            if(ch == 'R'){
+                x++;
+            }
+            else if(ch == 'L'){
+                x--;
+            }
+            else if(ch == 'U'){
+                y++;
+            }
+            else if(ch == 'D'){
+                y--;
+            }
+        }
+        return x == 0 && y==0;
+    }
+};
