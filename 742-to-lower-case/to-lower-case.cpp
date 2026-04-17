4f5dd4be77a8fff6e28630e6d68fb4c8b1d96a62
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-04-18 00:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (53.20%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        int n = s.size();
-        string ans = "";
-        for (int i = 0; i < n; i++) {
-            if((s[i] >= 'A') && (s[i] <= 'Z')){
-                char ch = char(s[i] + 32);
-                ans += ch;
-            }
-            else{
-                ans += s[i];
+        for(int i =0;i<s.size();i++){
+            if(s[i] >= 'A' && s[i] <= 'Z'){
+                s[i] = s[i] - 'A' + 'a';
             }
         }
-        return ans;
+        return s;
     }
 };
