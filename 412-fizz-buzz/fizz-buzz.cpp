fb1b0bc6df7fcfde3c4a2e0ee496f32b5ca0280e
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-04-30 19:36:28 +0530

    Time: 3 ms (33.68%) | Memory: 11.8 MB (17.76%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        int i = 1;
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.push_back("FizzBuzz");
+                i++;
+            } else if (i % 3 == 0) {
+                ans.push_back("Fizz");
+                i++;
+            } else if (i % 5 == 0) {
+                ans.push_back("Buzz");
+                i++;
+            } else {
+                ans.push_back(to_string(i));
+                i++;
+            }
+        }
+        return ans;
+    }
+};
