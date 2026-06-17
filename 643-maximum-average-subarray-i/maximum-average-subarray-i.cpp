ed422ed7529887d04f053115e034267a9473d31a
Vishal Prajapati <142673865+vishalprajapati15@users.noreply.github.com>
2026-06-17 15:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 113.7 MB (85.53%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         long long sum =0;
+        int n = nums.size();
         for(int i=0;i<k;i++){
             sum+= nums[i];
         }
         long long maxSum=sum;
-        for(int i=k;i<nums.size();i++){
-            sum = sum + nums[i] - nums[i-k];
+        for(int i=k;i<n;i++){
+            sum+= nums[i];
+            sum-= nums[i-k];
             maxSum = max(maxSum, sum);
         }
         return (double)maxSum/k;
